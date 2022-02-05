7962ad9d89f2812171069145d36cd46973148992
KoDenys <mr.kosyi@gmail.com>
2022-02-05 18:12:43 +0200

    Done 10** in HW1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 
 import com.epam.spring.homework1.config.BeansConfig;
 
+import com.epam.spring.homework1.pet.Cheetah;
 import com.epam.spring.homework1.pet.Pet;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,7 +10,12 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(BeansConfig.class);
         context.getBean(Pet.class).printPets();
+        System.out.println("-------------------");
+        Cheetah chtByClass = context.getBean(Cheetah.class);
+        Cheetah chtByName = (Cheetah) context.getBean("getBeanCheetahA");
 
+        System.out.println(chtByClass);
+        System.out.println(chtByName);
 
 
 
