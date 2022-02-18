8e524a2231d6f1aa61b550aba1f5070c0969aebf
KoDenys <98339173+KoDenys@users.noreply.github.com>
2022-02-18 15:46:00 +0200

    Update Dog.java

--- src/main/java/com/epam/spring/homework1/pet/Dog.java
+++ src/main/java/com/epam/spring/homework1/pet/Dog.java
@@ -4,7 +4,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
-@Order(1)
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class Dog implements Animal{
     public String getAnimal(){
         return this.getClass().getSimpleName();
